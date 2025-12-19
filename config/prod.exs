4542716ff70169c5a1efa9cda9550aa0ce90afa4
Gong Xue <gongxuemail@gmail.com>
2025-12-19 14:52:00 +0800

    fix: disable force_ssl

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ config :hello, HelloWeb.Endpoint, cache_static_manifest: "priv/static/cache_mani
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
 config :hello, HelloWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   exclude: [
     # paths: ["/health"],
     hosts: ["localhost", "127.0.0.1"]
